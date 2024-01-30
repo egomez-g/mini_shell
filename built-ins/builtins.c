317950f8db16b7fea8af56e50be7697823350c11
Santiago Gil Moya <sgil-moy@c2r19s6.42madrid.com>
2024-01-30 15:24:46 +0100

    aux solucionado

--- built-ins/builtins.c
+++ built-ins/builtins.c
@@ -1,38 +1,28 @@
 #include "../mini_shell.h"
 
-static char	*buitin_txt(char *txt, t_mini_shell *ms)
-{
-	char	*aux;
-	char	*expanded;
-
-	expanded = expand_variables(txt, ms);
-	aux = ft_strdup(expanded);
-	aux = clean_quotes(aux, ms);
-	return (aux);
-}
-
 int	do_builtins(char *txt, t_mini_shell *ms)
 {
 	char	**str;
 	char	*aux;
-
-	aux = buitin_txt(txt, ms);
+	
+	aux = expand_variables(txt, ms);
+	aux = clean_quotes(aux, ms);
 	str = ft_split(aux, ' ');
 	if (!str[0] || ft_strchr(aux, '|'))
 		return (free_strs(str), -1);
 	if (!(ft_strcmp(str[0], "cd")))
-		return (free_strs(str), free(aux), do_cd(aux, ms), 0);
+		return (free_strs(str), do_cd(aux, ms), free(aux), 0);
 	else if (!(ft_strcmp(str[0], "pwd")))
-		return (free_strs(str), free(aux), do_pwd(), 0);
+		return (free_strs(str), do_pwd(), free(aux), 0);
 	else if (!(ft_strcmp(str[0], "env")))
-		return (free_strs(str), free(aux), do_envp(ms), 0);
+		return (free_strs(str), do_envp(ms), free(aux), 0);
 	else if (!(ft_strcmp(str[0], "export")))
-		return (free_strs(str), free(aux), do_export(aux, ms), 0);
+		return (free_strs(str), do_export(aux, ms), free(aux), 0);
 	else if (!(ft_strcmp(str[0], "unset")))
-		return (free_strs(str), free(aux), do_unset(aux, ms), 0);
+		return (free_strs(str), do_unset(aux, ms), free(aux), 0);
 	else if (!(ft_strcmp(str[0], "echo")))
-		return (free_strs(str), free(aux), do_echo(aux, ms), 0);
+		return (free_strs(str), do_echo(aux, ms), free(aux), 0);
 	else if (!(ft_strcmp(str[0], "exit")))
-		return (free_strs(str), free(aux), do_exit(aux, ms), 0);
-	return (free_strs(str), free(aux), -1);
+		return (free_strs(str), do_exit(aux, ms), free(aux), 0);
+	return (free_strs(str), -1);
 }
